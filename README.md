6bfbcf35ebb8995854e5e20ff10d47b8d64ba44a
Artem Yakimenko <code@temik.me>
2015-05-23 20:46:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+zshmarks
+========
+
+A set of common functions used for enterprise Mac Packaging with Munki.
+
+How to install
+--------------
+
+oh-my-zsh
+---------
+* Download the script or clone this repository in [oh-my-zsh](http://github.com/robbyrussell/oh-my-zsh) plugins directory:
+
+        cd ~/.oh-my-zsh/custom/plugins
+        git clone git://github.com/temikus/mac-packaging.git
+
+* Activate the plugin in `~/.zshrc`:
+
+        plugins=( [plugins...] mac-packaging [plugins...])
+
+* Source `~/.zshrc`  to take changes into account:
+
+        source ~/.zshrc
+
+antigen
+-------
+Add `antigen bundle temikus/mac-packaging` to your .zshrc where you're adding your other plugins. Antigen will clone the plugin for you and add it to your antigen setup the next time you start a new shell.
+
+Commands/Usage:
+------
+
+* mkdmg - Makes a Munki-compatible DMG file out of an .app or pkg file and creates a manifest.
+
+        makedmg foo.[app|pkg]
+
+* mkmanifest - Generates a Munki manifest for a DMG file.
+
+        mkmanifest foo.dmg
+
+* check_appleid - prints unique user id for AppStore applications. Used to identify what user downloaded the application from the AppStore.
+
+        check_appleid foo.app
+
+Notes/Tips:
+-----------
+
+Plugin assumes that Munki tools are available in PATH:
+
+        export PATH=$PATH:/usr/local/munki
---
+++ mac-packaging.plugin.zsh
@@ -0,0 +1,64 @@
+###Functions###
+
+mkdmg()
+{
+
+  if [[ $1 =~ .pkg$ ]]; then
+    echo ".pkg file detected"
+    mkdmg_from_pkg $1
+  fi
+
+  if [[ $1 =~ .app$ ]]; then
+    echo ".app file detected"
+    mkdmg_from_app $1
+  else
+  	echo "Could not detect filetype, exiting..."
+  	exit 1
+  fi
+
+}
+
+mkdmg_from_app()
+{
+  BASENAME=`basename $1 .app`
+
+  echo "Creating packing environment..."
+  mkdir $BASENAME
+  mv $1 $BASENAME
+
+  echo "Creating DMG..."
+
+  hdiutil create -volname $BASENAME -srcfolder $BASENAME -uid 99 -gid 99 -ov -format UDZO -imagekey zlib-level=9  $BASENAME.dmg
+
+  echo "Creating manifest..."
+
+  makepkginfo $BASENAME.dmg > $BASENAME.plist
+}
+
+mkdmg_from_pkg()
+{
+  BASENAME=`basename $1 .pkg`
+
+  echo "Creating packing environment..."
+  mkdir $BASENAME
+  mv $1 $BASENAME
+
+  echo "Creating DMG..."
+
+  hdiutil create -volname $BASENAME -srcfolder $BASENAME -uid 99 -gid 99 -ov -format UDZO -imagekey zlib-level=9  $BASENAME.dmg
+
+  echo "Creating manifest..."
+
+  makepkginfo $BASENAME.dmg > $BASENAME.plist
+}
+
+mkmanifest()
+{
+	makepkginfo $1 > $1.plist
+}
+
+check_appleid()
+{
+  openssl asn1parse -inform der -in $1/Contents/_MASReceipt/receipt | grep -m 1 'OCTET STRING' | cut -d: -f4 | xxd -r -p > /tmp/app_payload.asn
+  printf "%d\n" 0x$(openssl asn1parse -inform der -in /tmp/app_payload.asn | grep -A 2 ':04$' | tail -1 | cut -d: -f4 | cut -c5-)
+}
